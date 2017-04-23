4889267a76bcbab75b1547c5001e25e371285971
igrokx70 <ivromanovich@gmail.com>
2017-04-23 18:21:37 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -22,38 +22,31 @@ $im1 = imagecreatefromjpeg($img);
 // Сохраняем изображение в 'simpl.jpg'
 imagejpeg($im1, "$id/simpl.jpg", 100);
 
-
-
-include('SimpleImage.php');    //Подключаем библиотеку
-   $image = new SimpleImage();    //Инициируем экземпляр класса
-   $image->load("$id/simpl.jpg");   //Загружаем фото (картинку)
-  $image->resampleToWidth(720);    //Изменяем размер со сглаживанием.
-   $image->sharpen();             //Придаем резкость (не обязательно)
-   $image->save("$id/simpl1.jpg");  //Сохраняем в файл
-
+include('SimpleImage.php');    
+   $image = new SimpleImage();   
+   $image->load("$id/simpl.jpg");   
+   $image->resampleToWidth(720);   
+   $image->sharpen();            
+   $image->save("$id/simpl1.jpg");  
 
 $im10 = imagecreatefromjpeg("$id/simpl1.jpg");
 $im20 = imagecreatefromjpeg('niz.jpg'); 
 $x = (imagesx($im10) - imagesx($im20)) / 2;
 $y = (imagesy($im10) - imagesy($im20)) / 1; 
-// Сохраняем альфа-канал.
-imagesavealpha($im10, true); 
-// Накладываем второе изображение на первое с нужным смещением
-imagecopy($im10, $im20, $x, $y, 0, 0, imagesx($im20), imagesy($im20));
-// Сохраняем изображение в 'simpl.jpg'
-imagejpeg($im10, "$id/simpl2.jpg", 100);
 
+imagesavealpha($im10, true); 
+imagecopy($im10, $im20, $x, $y, 0, 0, imagesx($im20), imagesy($im20));
+imagejpeg($im10, "$id/simpl2.jpg", 100);
 
 $im1 = imagecreatefromjpeg("$id/simpl2.jpg");
 $im2 = imagecreatefrompng('play13.png'); 
 $x = (imagesx($im1) - imagesx($im2)) / 2;
 $y = ((imagesy($im1) - imagesy($im2)) / 2) - 40; 
-// Сохраняем альфа-канал.
+
 imagesavealpha($im1, true); 
-// Накладываем второе изображение на первое с нужным смещением
 imagecopy($im1, $im2, $x, $y, 0, 0, imagesx($im2), imagesy($im2));
-// Сохраняем изображение в 'simpl3.jpg'
 imagejpeg($im1, "$id/simpl3.jpg", 100);
+
 $im = imagecreatefromjpeg("$id/simpl3.jpg");
 $x = 25;
 $y = ((imagesy($im10) - imagesy($im20)) / 1)+20; 
@@ -61,31 +54,22 @@ $y = ((imagesy($im10) - imagesy($im20)) / 1)+20;
 $white = imagecolorallocate($im, 255, 255, 255);
 $grey = imagecolorallocate($im, 128, 128, 128);
 $black = imagecolorallocate($im, 0, 0, 0);
-// Текст надписи
 $text = $title;
-// Замена пути к шрифту на пользовательский
 $font = './arial.ttf';
-// Текст
 imagefttext($im, 14, 0, $x, $y, $black, $font, $text);
-// Сохраняем изображение в 'simpl4.jpg'
 imagejpeg($im, "$id/simpl4.jpg", 100);
 
 $z = rand(3547, 32000);
 $im = imagecreatefromjpeg("$id/simpl4.jpg");
 $x = 25;
 $y = ((imagesy($im10) - imagesy($im20)) / 1)+40; 
-// Создание цветов
 $white = imagecolorallocate($im, 255, 255, 255);
 $grey = imagecolorallocate($im, 128, 128, 128);
 $black = imagecolorallocate($im, 0, 0, 0);
-// Текст надписи
 $text = "$z просмотров";
 $text = iconv('WINDOWS-1251', 'UTF-8', $text); 
-// Замена пути к шрифту на пользовательский
 $font = './arial.ttf';
-// Текст
 imagefttext($im, 12, 0, $x, $y, $grey, $font, $text);
-// Сохраняем изображение в 'simpl4.jpg'
 imagejpeg($im, "$id/simpl4.jpg", 100);
 
 unlink ("$id/simpl.jpg");
@@ -98,10 +82,11 @@ a:
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <meta http-equiv="Content-Type" content="text/html; charset=utf8" />
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#" xml:lang="ru" lang="ru">
-<head>       
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-  <title><?php echo($title);?></title> 
+<head>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>  
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title><?php echo($title);?></title> 
   <meta property="og:image" content="/Urez/<?php echo($id);?>/simpl4.jpg" />
   <meta property="og:url" content="http://www.dailymotion.com/video/<?php echo($id);?>" />
   <meta property="og:video:iframe" content="http://www.dailymotion.com/swf/video/<?php echo($id);?>?autoPlay=1" />
