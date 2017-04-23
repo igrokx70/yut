d22e693912877edc74378f6c57284218ccad3bde
igrokx70 <ivromanovich@gmail.com>
2017-04-23 18:04:12 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -6,11 +6,8 @@ $res = preg_match("/<title>(.*) - Р’РёРґРµРѕ Dailymotion<\/title>/siU", $content
 $title = preg_replace('/\s+/', ' ', $title_matches[1]);
 $id = $_GET['id'];
 
-
 if (file_exists($id)) {
     goto a;
-
-
 
 } else {
     goto b;
@@ -57,9 +54,6 @@ imagesavealpha($im1, true);
 imagecopy($im1, $im2, $x, $y, 0, 0, imagesx($im2), imagesy($im2));
 // Сохраняем изображение в 'simpl3.jpg'
 imagejpeg($im1, "$id/simpl3.jpg", 100);
-
-
-
 $im = imagecreatefromjpeg("$id/simpl3.jpg");
 $x = 25;
 $y = ((imagesy($im10) - imagesy($im20)) / 1)+20; 
@@ -69,14 +63,12 @@ $grey = imagecolorallocate($im, 128, 128, 128);
 $black = imagecolorallocate($im, 0, 0, 0);
 // Текст надписи
 $text = $title;
-
 // Замена пути к шрифту на пользовательский
 $font = './arial.ttf';
 // Текст
 imagefttext($im, 14, 0, $x, $y, $black, $font, $text);
 // Сохраняем изображение в 'simpl4.jpg'
 imagejpeg($im, "$id/simpl4.jpg", 100);
-
 
 $z = rand(3547, 32000);
 $im = imagecreatefromjpeg("$id/simpl4.jpg");
@@ -96,46 +88,30 @@ imagefttext($im, 12, 0, $x, $y, $grey, $font, $text);
 // Сохраняем изображение в 'simpl4.jpg'
 imagejpeg($im, "$id/simpl4.jpg", 100);
 
-
-
-
 unlink ("$id/simpl.jpg");
 unlink ("$id/simpl1.jpg");
 unlink ("$id/simpl2.jpg");
 unlink ("$id/simpl3.jpg");
-
-
 a:
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <meta http-equiv="Content-Type" content="text/html; charset=utf8" />
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#" xml:lang="ru" lang="ru">
-<head>
-    
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-    
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title><?php echo($title);?></title> 
-
-   <meta property="og:image" content="/Urez/<?php echo($id);?>/simpl4.jpg" />
+<head>       
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <title><?php echo($title);?></title> 
+  <meta property="og:image" content="/Urez/<?php echo($id);?>/simpl4.jpg" />
   <meta property="og:url" content="http://www.dailymotion.com/video/<?php echo($id);?>" />
   <meta property="og:video:iframe" content="http://www.dailymotion.com/swf/video/<?php echo($id);?>?autoPlay=1" />
   <meta property="og:video:type" content="application/x-shockwave-flash" />
   <meta property="og:video:width" content="720" />
   <meta property="og:video:height" content="410" />
-   <link rel="canonical" href="http://www.dailymotion.com/video/<?php echo($id);?>" />
-<meta property="og:title" content="<?php echo($title);?>" />
-
+  <link rel="canonical" href="http://www.dailymotion.com/video/<?php echo($id);?>" />
+  <meta property="og:title" content="<?php echo($title);?>" />
 </head>
 <body onresize="onBodyResize()" class="is_rtl font_default pads ">
 <center><iframe frameborder="0" width="640" height="480" src="//www.dailymotion.com/embed/video/<?php echo($id);?>?autoplay=1" allowfullscreen></iframe><br /></center>
-
-<center>
-        
-</center> 
-
-
 </body>
 </html>
